fb7fb05cef5ab3067bb98704098b95b3ae142daf
Jonasoft <carlosucananbarrera@hotmail.com>
2020-01-07 17:00:12 -0500

    preproduccion proyectoflights final4

--- client_dev/src/app/servicios/data.service.ts
+++ client_dev/src/app/servicios/data.service.ts
@@ -20,7 +20,7 @@ export class DataService {
   peticion:any;
 
 
-  URL_API = '/flights';
+  URL_API = 'flights';
  
   constructor(private http:HttpClient) {
     this.selectedFlight = new Flights()
